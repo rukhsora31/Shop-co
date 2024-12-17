bd633cd06b7d173dd8e3ee4ae813268bc6c71562
ruxsora8 <ruhsora447@gmail.com>
2024-12-17 16:57:43 +0500

    Add files via upload

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,122 +1,78 @@
-// import React from 'react'
-// import './Header.scss'
-// import { Link } from 'react-router-dom'
-
-// const Header = () => {
-//   return (
-//    <>
-//    <section className="header">
-//     <div className="container">
-
-//         <nav>
-//         {/* <img className='logo' src="/SHOP.CO.png" alt="" /> */}
-//         <Link to={'/'} className='logo'>
-//         <img className='logo' src="/SHOP.CO.png" alt="" />
-
-//         </Link>
-
-//         <div className="menu">
-//             <Link to={'/sale'} className='dropdown'>
-//             Shop
-//             <img src="https://www.iconpacks.net/icons/2/free-arrow-down-icon-3101-thumb.png" alt="" />
-
-//             <div>
-//                 <Link to={'/'}>Man</Link>
-//                 <Link to={'/'}>Women</Link>
-//                 <Link to={'/'}>Kids</Link>
-//             </div>
-
-//             </Link>
-//             <a href="/#selling">On sale</a>
-//             <a href="/#arrivals">New Arrivals</a>
-//             <Link to={'/'}>Brands</Link>
-//         </div>
-
-//         <input type="text" className="search" />
-
-//         <div className="icons">
-//             <img src="/icon-1.svg" alt="" />
-//             <img src="/icon-2.svg" alt="" />
-//         </div>
-
-//         </nav>
-
-//     </div>
-//    </section>
-//    </>
-//   )
-// }
-
-// export default Header
-
-import React, { useState } from 'react'
-import './Header.scss'
-import { Link } from 'react-router-dom'
-import { useAppContext } from '../../context'
+import React, { useEffect } from 'react';
+import './Header.scss';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import { Link } from 'react-router-dom';
+import { useAppContext } from '../../context';
 
 const Header = () => {
+    const { isActive, toggleBurger, user, isAuth, logout } = useAppContext();
 
-    const { isActive, toggleBurger, user, isAuth, logout } = useAppContext()
+    // Инициализация AOS
+    useEffect(() => {
+        AOS.init({
+            duration: 1000, // Длительность анимации
+            easing: 'ease-in-out', // Плавность анимации
+            once: true, // Анимация запускается один раз
+        });
+    }, []);
 
     return (
         <>
             <header className="header">
                 <div className="container">
-                    <div className='header__wrapper'>
+                    <div className="header__wrapper" data-aos="fade-down" data-aos-delay="100">
                         <div className="header__wrap">
                             <div className="header__header">
-                                <Link to={'/'}><img className='logo__img' src="/SHOP.CO.png" alt="" /></Link>
-                                <div className={`header__menu ${isActive ? 'active' : ''}`}>
-                                    <Link to={'/sale'} className='dropdown' >
+                                <Link to={'/'} data-aos="fade-down" data-aos-delay="200">
+                                    <img className='logo__img' src="/SHOP.CO.png" alt="" />
+                                </Link>
+                                <div className={`header__menu ${isActive ? 'active' : ''}`} data-aos="fade-down" data-aos-delay="300">
+                                    <Link to={'/sale'} className='dropdown' data-aos="fade-down" data-aos-delay="400">
                                         Shop
                                         <img src="https://www.iconpacks.net/icons/2/free-arrow-down-icon-3101-thumb.png" alt="" />
-
                                         <div>
-                                            <Link to={'/'}>Men</Link>
-                                            <Link to={'/'}>Women</Link>
-                                            <Link to={'/'}>Kids</Link>
-
+                                            <Link to={'/'} data-aos="fade-down" data-aos-delay="500">Men</Link>
+                                            <Link to={'/'} data-aos="fade-down" data-aos-delay="600">Women</Link>
+                                            <Link to={'/'} data-aos="fade-down" data-aos-delay="700">Kids</Link>
                                         </div>
                                     </Link>
-
-                                    <a href="/#selling">On Sale</a>
-                                    <a href="/#arrivals">New Arrivals</a>
-                                    <Link to={'/brands'}>Brands</Link>
+                                    <a href="/#selling" data-aos="fade-down" data-aos-delay="800">On Sale</a>
+                                    <a href="/#arrivals" data-aos="fade-down" data-aos-delay="900">New Arrivals</a>
+                                    <Link to={'/brands'} data-aos="fade-down" data-aos-delay="1000">Brands</Link>
                                 </div>
                             </div>
 
-                            <div onClick={toggleBurger} className={`burger ${isActive ? 'active' : ''}`}>
+                            <div onClick={toggleBurger} className={`burger ${isActive ? 'active' : ''}`} data-aos="fade-down" data-aos-delay="1100">
                                 <div className='div'></div>
                                 <div className='div'></div>
                                 <div className='div'></div>
                             </div>
                         </div>
 
-                        <div className="header__search">
-                            <div className='header__input'>
+                        <div className="header__search" data-aos="fade-down" data-aos-delay="1200">
+                            <div className='header__input' data-aos="fade-down" data-aos-delay="1300">
                                 <img src="/search-icon.svg" alt="" />
                                 <p>Search for products...</p>
                             </div>
-                            <img src="/icon-1.svg" alt="" />
+                            <img src="/icon-1.svg" alt="" data-aos="fade-down" data-aos-delay="1400" />
 
                             {isAuth ? (
-                                <div>
-                                <b>{user.username}</b> <br />
-                                <button onClick={logout}>logout</button>
+                                <div data-aos="fade-down" data-aos-delay="1500">
+                                    <b>{user.username}</b> <br />
+                                    <button onClick={logout}>Logout</button>
                                 </div>
                             ) : (
-                                <Link to={'/login'}>
+                                <Link to={'/login'} data-aos="fade-down" data-aos-delay="1600">
                                     <img src="/icon-2.svg" alt="" />
                                 </Link>
                             )}
-
-
                         </div>
                     </div>
                 </div>
             </header>
         </>
-    )
+    );
 }
 
-export default Header
+export default Header;
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,40 +1,66 @@
-import React from 'react'
-import './Hero.scss'
+import React, { useEffect } from 'react';
+import './Hero.scss';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 const Hero = () => {
+  // Инициализация AOS
+  useEffect(() => {
+    AOS.init({
+      duration: 1000, // Длительность анимации
+      easing: 'ease-in-out', // Плавность анимации
+      once: true, // Анимация срабатывает один раз
+    });
+  }, []);
+
   return (
-  <>
-  <section className="hero">
-    <div className="container">
-        <div className="hero__wrapper">
-            <div className="hero__box">
-                <h1>FIND CLOTHES THAT MATCHES YOUR STYLE</h1>
-                <p>Browse through our diverse range of meticulously crafted garments, designed to bring out your individuality and cater to your sense of style.</p>
-                <button>Shop Now</button>
+    <>
+      <section className="hero">
+        <div className="container">
+          <div className="hero__wrapper">
+            {/* Левая часть */}
+            <div className="hero__box" data-aos="fade-right">
+              <h1 data-aos="fade-up" data-aos-delay="100">
+                FIND CLOTHES THAT MATCHES YOUR STYLE
+              </h1>
+              <p data-aos="fade-up" data-aos-delay="200">
+                Browse through our diverse range of meticulously crafted garments, 
+                designed to bring out your individuality and cater to your sense of style.
+              </p>
+              <button data-aos="zoom-in" data-aos-delay="300">Shop Now</button>
 
-                <div className="hero__inner">
-                    <div>
-                        <h3>200+</h3>
-                        <p>International Brands</p>
-                    </div>
-
-                    <div>
-                        <h3>2,000+</h3>
-                        <p>High-Quality Products</p>
-                    </div>
-
-                    <div>
-                        <h3>30,000+</h3>
-                        <p>Happy Customers</p>
-                    </div>
+              {/* Статистика */}
+              <div className="hero__inner" data-aos="fade-up" data-aos-delay="400">
+                <div>
+                  <h3 data-aos="fade-up" data-aos-delay="500">200+</h3>
+                  <p data-aos="fade-up" data-aos-delay="600">International Brands</p>
                 </div>
-            </div>
-            <img src="/hero-img.png" alt="" />
-        </div>
-    </div>
-  </section>
-  </>
-  )
-}
 
-export default Hero
+                <div>
+                  <h3 data-aos="fade-up" data-aos-delay="700">2,000+</h3>
+                  <p data-aos="fade-up" data-aos-delay="800">High-Quality Products</p>
+                </div>
+
+                <div>
+                  <h3 data-aos="fade-up" data-aos-delay="900">30,000+</h3>
+                  <p data-aos="fade-up" data-aos-delay="1000">Happy Customers</p>
+                </div>
+              </div>
+            </div>
+
+            {/* Правая часть: изображение */}
+            <img
+              src="/hero-img.png"
+              alt="Hero"
+              data-aos="fade-left"
+              data-aos-delay="500"
+            />
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Hero;
+
